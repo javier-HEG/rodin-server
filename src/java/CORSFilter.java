40c286ce83c1036112bfa29b2200302748e82253
Javier Belmonte <javier.belmonte@hesge.ch>
2013-09-11 15:06:29 +0200

    Reduces the security built for CORS but provides first step to programatically controlling the CORS access

--- src/java/CORSFilter.java
+++ src/java/CORSFilter.java
@@ -7,8 +7,8 @@ public class CORSFilter implements ContainerResponseFilter {
 
 	@Override
 	public ContainerResponse filter(ContainerRequest creq, ContainerResponse cresp) {
-
-		cresp.getHttpHeaders().putSingle("Access-Control-Allow-Origin", "http://localhost");
+		// TODO Check the Origin before allowing cross-reference calls
+		cresp.getHttpHeaders().putSingle("Access-Control-Allow-Origin", creq.getHeaderValue("Origin"));
 		cresp.getHttpHeaders().putSingle("Access-Control-Allow-Credentials", "true");
 		cresp.getHttpHeaders().putSingle("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
 		cresp.getHttpHeaders().putSingle("Access-Control-Allow-Headers", "Content-Type, Accept");
